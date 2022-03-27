626ea9ba1759ccdfdd702d9080447b540a1169ed
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-27 17:08:26 +0000

    changed index.html

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* Created by: Venika Sem
+ * Created on: Mar 2022
+ * This file contains the JS functions for index.html 
+*/
+
+'use strict'
+/**
+ * This function calculates the pay and government tax of user.
+ */
+function pay () {
+  // input
+  const numberOfHoursWorked = parseInt(document.getElementById('number-of-hours-worked').value)
+  const hourlyRate = parseInt(document.getElementById('hourly-rate').value)
+
+   // process
+  const takeHomeSalary = (numberOfHoursWorked * hourlyRate) * (1.00 - 0.18)
+  const governmentTax = (numberOfHoursWorked * hourlyRate) * 0.18
+
+  // output
+  document.getElementById('take-home-salary').innerHTML = 'Your pay will be: $' + takeHomeSalary.toFixed(2)
+  document.getElementById('government-tax').innerHTML = 'The government will take: $' + governmentTax.toFixed(2)
+}
